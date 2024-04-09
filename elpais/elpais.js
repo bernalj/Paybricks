25442f9285159788723c9dee81840324616ce847
Javiotic <xlonadarx@gmail.com>
2024-04-09 09:39:46 +0200

    Clean up code and make it modular
    Moved the functions of each newspaper to their own folder, and calling them dynamically from contents.js

---
+++ elpais/elpais.js
@@ -0,0 +1,24 @@
+// Get references to the premium and freemium article divs
+var premiumDiv = document.getElementById("ctn_premium_article");
+var freemiumDiv = document.getElementById("ctn_freemium_article");
+
+// If the premiumDiv exists, remove its outer HTML (content)
+if (premiumDiv) {
+  premiumDiv.outerHTML = "";
+}
+
+// If the freemiumDiv exists, remove its outer HTML (content)
+if (freemiumDiv) {
+  freemiumDiv.outerHTML = "";
+}
+
+// Get the element with class "a_b_wall _dn" (assuming it represents the rest of the article)
+var restOfArticle = document.getElementsByClassName("a_b_wall _dn")[0];
+
+// If restOfArticle is found, update its class name to "a_c clearfix"
+if (restOfArticle) {
+  restOfArticle.className = "a_c clearfix";
+}
+
+// Log a message indicating that the paywall has been removed (assuming messages.PAYWALL_REMOVED is defined elsewhere)
+console.log(messages.PAYWALL_REMOVED);
---
+++ elperiodico/elperiodico.js
@@ -0,0 +1,29 @@
+// Function to remove specified text from a string
+function removeText(originalString, textToRemove) {
+  // Split the original string by the text to remove
+  let parts = originalString.split(textToRemove);
+  
+  // Join the parts back together without the removed text
+  return parts.join('');
+}
+
+// Set innerHTML of element with id "paywall" to an empty string
+document.getElementById("paywall").innerHTML = "";
+
+// Remove class name from element with class "ft-helper-closenews"
+var newsContainer = document.getElementsByClassName("ft-helper-closenews")[0];
+if (newsContainer) {
+  newsContainer.className = "";
+}
+
+// Remove "closeContentEnd" from class names of elements with the class "closeContentEnd"
+var censoredParagraphs = document.getElementsByClassName("closeContentEnd");
+
+for (var i = 0; i < censoredParagraphs.length; i++) {
+  var divClass = censoredParagraphs[i].className;
+
+  let cleanedClass = removeText(divClass, "closeContentEnd");
+  
+  // Update the className property of the element to remove "closeContentEnd"
+  censoredParagraphs[i].className = cleanedClass.trim(); // Trim to remove leading/trailing spaces
+}
