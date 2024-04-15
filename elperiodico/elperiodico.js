496fc6c69aefa684cb9be47c644fb24ab03835da
Javiotic <xlonadarx@gmail.com>
2024-04-15 09:34:28 +0200

    Fix removing the div with subscription prices, and organizes the code

--- elperiodico/elperiodico.js
+++ elperiodico/elperiodico.js
@@ -1,3 +1,7 @@
+//
+// Helper Functions
+//
+
 // Function to remove specified text from a string
 function removeText(originalString, textToRemove) {
   // Split the original string by the text to remove
@@ -7,19 +11,44 @@ function removeText(originalString, textToRemove) {
   return parts.join('');
 }
 
-// Set innerHTML of element with id "paywall" to an empty string
-
-var paywallContainer = document.getElementById("paywall");
-
-if (paywallContainer){
-  document.getElementById("paywall").innerHTML = "";
-}
+//
+// Instant Functions
+//
 
 // Remove class name from element with class "ft-helper-closenews"
 var newsContainer = document.getElementsByClassName("ft-helper-closenews")[0];
 if (newsContainer) {
   newsContainer.className = "";
 }
+
+// Remove "closeContentEnd" from class names of elements with the class "closeContentEnd"
+// Iterates through all the paragraphs of an article and shows them
+var censoredParagraphs = document.getElementsByClassName("closeContentEnd");
+
+for (var i = 0; i < censoredParagraphs.length; i++) {
+  var divClass = censoredParagraphs[i].className;
+
+  let cleanedClass = removeText(divClass, "closeContentEnd");
+  
+  // Update the className property of the element to remove "closeContentEnd"
+  censoredParagraphs[i].className = cleanedClass.trim(); // Trim to remove leading/trailing spaces
+}
+
+//
+// Timed functions
+//
+
+function removePaywallDiv(){
+  // Set innerHTML of element with id "paywall" to an empty string
+  var paywallContainer = document.getElementById("paywall");
+
+  if (paywallContainer){
+    document.getElementById("paywall").innerHTML = "";
+  }
+}
+
+setTimeout(removePaywallDiv, 1000);
+
 
 function removeReportajePaysplash() {
   // Get the first element with the specified class name
@@ -39,18 +68,6 @@ function removeReportajePaysplash() {
 
 // Set a timeout to call the removeReportajePaysplash function after 1000 milliseconds (1 second)
 setTimeout(removeReportajePaysplash, 1000);
-
-// Remove "closeContentEnd" from class names of elements with the class "closeContentEnd"
-var censoredParagraphs = document.getElementsByClassName("closeContentEnd");
-
-for (var i = 0; i < censoredParagraphs.length; i++) {
-  var divClass = censoredParagraphs[i].className;
-
-  let cleanedClass = removeText(divClass, "closeContentEnd");
-  
-  // Update the className property of the element to remove "closeContentEnd"
-  censoredParagraphs[i].className = cleanedClass.trim(); // Trim to remove leading/trailing spaces
-}
 
 // Log a message indicating that the paywall has been removed
 console.log("Paywall removed for " + window.location.href);
